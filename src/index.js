8dee9daf184e9fc4ce0415156cc79d445cdfa12b
CrazyDog1811 <evgeshatertyshnyi@mail.ru>
2021-12-19 18:30:13 +0300

    feat: make a function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const BRACKET_MAP = {};
+  const OPEN_BRACKETS = [];
+  const STACK = [];
+
+  bracketsConfig.forEach(element => {
+    BRACKET_MAP[element[1]] = element[0];
+    OPEN_BRACKETS.push(element[0]);
+  });
+
+  for(let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if(OPEN_BRACKETS.includes(currentSymbol)) {
+
+      if(currentSymbol === '|') {
+        if(STACK.includes(currentSymbol)) {
+          if(STACK[STACK.length -1] === currentSymbol) {
+            STACK.pop();
+            continue;
+          } else {
+            return false;
+          }
+        }
+      }
+      
+       STACK.push(currentSymbol);
+   
+    } else {
+
+      if(STACK.length === 0) return false;
+
+      let stackTopSymbol = STACK[STACK.length - 1];
+
+      if(BRACKET_MAP[currentSymbol] === stackTopSymbol) {
+        STACK.pop();
+      } else {
+        return false;
+      }
+
+
+    }
+  }
+
+  return STACK.length === 0;
+
 }
